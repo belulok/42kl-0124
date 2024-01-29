780de25784eef3044a3cccb11654f199ca8e29b7
jmdev-sebas <sebastian.belulok@junomarkets.com>
2024-01-29 17:18:41 +0800

    fixed

--- c/c02-l/ex09/ft_strcapitalize.c
+++ c/c02-l/ex09/ft_strcapitalize.c
@@ -12,25 +12,16 @@
 
 char	*ft_strcapitalize(char *str)
 {
-	int	c;
 	int	i;
 
-	c = 0;
-	i = 0;
-	while (str[c] != '\0')
+	i = 1;
+	if ((str[0] >= 'a') && (str[0] <= 'z'))
+		str[0] -= 32;
+	while (str[i])
 	{
-		if (i == 0 && str[c] >= 'a' && str[c] <= 'z')
-		{
-			str[c] -= 32;
-			i++;
-		}
-		else if (i > 0 && str[c] >= 'A' && str[c] <= 'Z')
-			str[c] += 32;
-		else if (i > 0 && str[c] == ' ')
-			i = 0;
-		else
-			i++;
-		c++;
+		if ((str[i - 1] < 47) && ((str[i] >= 'a') && (str[i] <= 'z')))
+			str[i] -= 32;
+		i++;
 	}
 	return (str);
 }

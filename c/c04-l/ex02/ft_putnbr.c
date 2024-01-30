192106bfd29cc6f0a41939e8dc940d7a2a756824
jmdev-sebas <sebastian.belulok@junomarkets.com>
2024-01-30 12:44:06 +0800

    fix

--- c/c04-l/ex02/ft_putnbr.c
+++ c/c04-l/ex02/ft_putnbr.c
@@ -10,6 +10,8 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+
 void	ft_putchar(char c)
 {
 	write(1, &c, 1);
@@ -17,23 +19,30 @@ void	ft_putchar(char c)
 
 void	ft_putnbr(int nb)
 {
-	if (nb < 0)
+	if (nb == -2147483648)
 	{
 		ft_putchar('-');
-		nb *= -1;
+		ft_putchar('2');
+		nb = 147483648; // Remainder of the number after taking out '-2'
+	}
+	else if (nb < 0)
+	{
+		ft_putchar('-');
+		nb = -nb;
 	}
 	if (nb < 10)
 	{
-		ft_putchar(nb + 48);
-		return ;
+		ft_putchar(nb + '0');
 	}
 	else
+	{
 		ft_putnbr(nb / 10);
-	ft_putnbr(nb % 10);
+		ft_putnbr(nb % 10);
+	}
 }
 
 /*
-int main(void)
+int	main(void)
 {
 	ft_putnbr(42);
 	return (0);

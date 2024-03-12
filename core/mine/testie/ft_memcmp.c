963593aad42acce2250ae2f6fd069d358debfe21
belulok <belulok_sebastian@hotmail.com>
2024-03-12 20:00:35 +0800

    update

--- core/mine/testie/ft_memcmp.c
+++ core/mine/testie/ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: sesaging <sesaging@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/09 21:41:35 by sesaging          #+#    #+#             */
-/*   Updated: 2024/03/09 21:42:25 by sesaging         ###   ########.fr       */
+/*   Updated: 2024/03/12 19:49:39 by sesaging         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	const unsigned char	*p1;
 	const unsigned char	*p2;
 
-	*p1 = s1;
-	*p2 = s2;
+	p1 = s1;
+	p2 = s2;
 	while (n-- > 0)
 	{
 		if (*p1 != *p2)
--- core/mine/testie/ft_memcpy.c
+++ core/mine/testie/ft_memcpy.c
@@ -6,20 +6,24 @@
 /*   By: sesaging <sesaging@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/09 20:05:18 by sesaging          #+#    #+#             */
-/*   Updated: 2024/03/09 20:07:53 by sesaging         ###   ########.fr       */
+/*   Updated: 2024/03/12 19:58:20 by sesaging         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-include "libft.h"
+#include "libft.h"
 
 void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t	n)
 {
 	unsigned char		*byteleveldst;
 	const unsigned char	*bytelevelsrc;
 
+	byteleveldst = (unsigned char *)dst;
+	bytelevelsrc = (const unsigned char *)src;
+	if (dst == NULL || src == NULL)
+		return (NULL);
 	while (n--)
 	{
-		*byteleveldst = bytelevelsrc;
+		*byteleveldst++ = *bytelevelsrc++;
 	}
 	return (dst);
 }
